d1e1f5480001ecd91d438deb6816a3e9ccb85be0
Kano85 <afte_85@hotmail.com>
2024-11-26 13:54:12 +0100

    Component separation slideup & preloader

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -2,9 +2,15 @@
 
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { motion } from 'motion/react'; // Ensure consistent import
 import styles from './style.module.scss';
+import gsap from 'gsap';
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin'; // Adjust the path as necessary
+import SlideUp from '../SlideUp'; // Import the SlideUp component
+
+// Register the ScrambleTextPlugin
+gsap.registerPlugin(ScrambleTextPlugin);
 
 // Animation configurations
 const opacity = {
@@ -13,66 +19,44 @@ const opacity = {
   },
   enter: {
     opacity: 0.75,
-    transition: { duration: 1, delay: 0.2 },
+    transition: { duration: 2, delay: 0.2 },
   },
 };
-
-const slideUp = {
-  initial: {
-    top: 0,
-  },
-  enter: {
-    top: 0,
-  },
-  exit: {
-    top: '-100vh',
-    transition: {
-      duration: 0.8,
-      ease: [0.76, 0, 0.24, 1],
-      delay: 0.2,
-    },
-  },
-};
-
-// List of words to display
-const words: string[] = [
-  'Hello',
-  'Bonjour',
-  'Ciao',
-  'Olà',
-  'やあ',
-  'Hallå',
-  'Guten tag',
-  'Hallo',
-];
 
 const Preloader: React.FC = () => {
-  const [index, setIndex] = useState<number>(0);
+  const scrambleRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
-    if (index === words.length - 1) return;
-    const timeout = setTimeout(
-      () => {
-        setIndex(index + 1);
-      },
-      index === 0 ? 1000 : 150
-    );
-    return () => clearTimeout(timeout);
-  }, [index]);
+    if (scrambleRef.current) {
+      const tl = gsap.timeline({ defaults: { duration: 2, ease: 'none' } });
+
+      tl.to(scrambleRef.current, {
+        duration: 1,
+        scrambleText: {
+          text: 'Animate',
+          chars: 'lowerCase',
+          revealDelay: 0.5,
+          tweenLength: false,
+        },
+      }).to(scrambleRef.current, {
+        duration: 1,
+        scrambleText: {
+          text: "Specify'XO'",
+          chars: 'XO',
+          revealDelay: 1,
+          tweenLength: false,
+          speed: 0.4,
+        },
+      });
+    }
+  }, []);
 
   return (
-    <motion.div
-      variants={slideUp}
-      initial="initial"
-      animate="enter"
-      exit="exit"
-      className={styles.introduction}
-    >
+    <SlideUp className={styles.introduction}>
       <motion.p variants={opacity} initial="initial" animate="enter">
-        <span></span>
-        {words[index]}
+        <span ref={scrambleRef}></span>
       </motion.p>
-    </motion.div>
+    </SlideUp>
   );
 };
 
