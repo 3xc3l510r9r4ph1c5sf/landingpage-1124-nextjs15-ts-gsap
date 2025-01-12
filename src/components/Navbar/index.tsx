6783ab839eadb252242407f744a649a701cf31bf
Kano85 <afte_85@hotmail.com>
2025-01-12 23:36:52 +0100

    Refactor Navbar component: unify string quotes and remove text content from NavLink elements

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,14 @@
-import NavLink from "./MagneticNavlink";
+import NavLink from './MagneticNavlink';
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-0 h-[var(--navbar-height)] bg-hero-dark pl-[4.37rem] pt-[1.69rem] text-mainbody-weg">
       <ul className="flex gap-12">
-        <NavLink href="/">Works</NavLink>
-        <NavLink href="contact">Contact</NavLink>
-        <NavLink icons href="/">
-          Hallo
-        </NavLink>
+        <NavLink href="/"> </NavLink>
+        <NavLink href="contact"> </NavLink>
+        {/* <NavLink icons href="/">
+          {' '}
+        </NavLink> */}
       </ul>
     </nav>
   );
